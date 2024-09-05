7277efeacbaac5e8269dadb1c1a05a27286ed212
284km <k.furuhashi10@gmail.com>
2024-09-05 23:47:45 +0900

    Add a smple root helloworld endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3051").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+}
+
