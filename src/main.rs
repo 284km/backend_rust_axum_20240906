7c0e6a0424ae5949eda6d0d8cfa4fcfa1e6b4e87
284km <k.furuhashi10@gmail.com>
2024-09-06 11:23:35 +0900

    sample post api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use axum::{Router, routing::get};
+use axum::{
+    http::StatusCode, routing::{get, post}, Json, Router
+};
+use serde::{Deserialize, Serialize};
 
 async fn root() -> &'static str {
     "Hello, World!"
@@ -7,10 +10,39 @@ async fn root() -> &'static str {
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/users", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3051").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
 }
 
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1001,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
